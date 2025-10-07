dbb94596220b9c0b0d2710a927b05cf4562db4a9
Dimpal Yadav <dimpal.yadav2912@gmail.com>
2025-10-07 17:42:04 +0530

    Update user.route.js

--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { signUp, logIn, getUserDetails, refreshToken } from "../Controllers/user.controller.js";
+import { signUp, logIn, getUserDetails, refreshToken, requestPasswordReset,  resetPassword } from "../Controllers/user.controller.js";
 import authenticateToken from "../Middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", logIn);
+
 router.get("/api/v1/user/:id/details", authenticateToken, getUserDetails);
 router.post("/api/v1/user/:id/refreshToken", authenticateToken, refreshToken)
+
+// ✅ New routes for Forgot Password flow
+router.post("/auth/forgot-password", requestPasswordReset); // request reset link
+router.post("/auth/reset-password", resetPassword);         // submit new password
 
 export default router;
