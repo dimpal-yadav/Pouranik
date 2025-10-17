fe8fc3fb821a5934b99053e320f3513193886317
Dimpal Yadav <dimpal.yadav2912@gmail.com>
2025-10-18 00:31:46 +0530

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from '../pages/Home';
 import BookDetail from '../pages/BookDetail';
 import Genres from '../pages/Genres';
-import { Routes, Route } from "react-router-dom";
-import Home from '../pages/Home';
 import Explore from '../pages/Explore';
 import AboutUs from '../pages/about';
 import Library from '../pages/Library';
@@ -11,10 +13,11 @@ import Community from '../pages/Community';
 import ClubPage from '../pages/ClubPage';
 import TimerPage from '../pages/TimerPage';
 import AnalyticsPage from '../pages/AnalyticsPage';
+import ForgotPassword from '../pages/ForgotPassword'; // ✅ Forgot Password Page
+
+// Components
 import NoBookFound from '../components/NoBookFound';
 import NotFoundPage from '../components/Notfound';
-// ✅ **NEW IMPORT FOR FORGOT PASSWORD PAGE**
-import ForgotPassword from '../pages/ForgotPassword';
 
 export default function AppRoutes() {
   return (
@@ -24,16 +27,15 @@ export default function AppRoutes() {
       <Route path="/genres" element={<Genres />} />
       <Route path="/explore" element={<Explore />} />
       <Route path="/about" element={<AboutUs />} />
-      <Route path='/library' element={<Library />} />
-      <Route path='/timerpage' element={<TimerPage/>} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/timerpage" element={<TimerPage />} />
       <Route path="/analytics" element={<AnalyticsPage />} />
-      <Route path='/signup' element={<SignIn />} />
-      <Route path='/book/:id/reviews' element={<Reviews />} />
-      <Route path='/community' element={<Community />} />
-      <Route path='/club' element={<ClubPage />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />  {/* ✅ NEW ROUTE ADDED */}
+      <Route path="/signup" element={<SignIn />} />
+      <Route path="/book/:id/reviews" element={<Reviews />} />
+      <Route path="/community" element={<Community />} />
+      <Route path="/club" element={<ClubPage />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} /> {/* ✅ NEW ROUTE */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
-
 }
